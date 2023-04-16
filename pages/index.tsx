e777c511f4c6ff65bf4bae653d3d375796fa78cd
Nuri <for9595@google.com>
2023-04-16 16:42:51 +0900

    매장 데이터 불러오기(feat. SWR)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,30 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import React from 'react';
 import Link from 'next/link';
+import { NextPage } from 'next';
 import styles from 'styles/header.module.scss';
 
 import Header from '../components/common/Header';
 import MapSection from '@components/home/MapSection';
 
+import useStores from '@hooks/useStores';
+
 import { AiOutlineShareAlt } from 'react-icons/ai';
 import { VscFeedback } from 'react-icons/vsc';
 
-export default function Home() {
+import { Store } from 'types/store';
+
+interface Props {
+  stores: Store[];
+}
+
+const Home: NextPage<Props> = ({ stores }) => {
+  const { initializeStores } = useStores(); // 훅으로 선언한 함수를 가져옴.
+
+  useEffect(() => {
+    initializeStores(stores); // 새로운 매장 데이터가 들어왔을 때, stores로 인자를 주었으니 hooks로 넘어가 STORE_KEY에 전역상태로 관리?
+  }, [initializeStores, stores]);
+
   return (
     <Fragment>
       <Header
@@ -39,4 +54,16 @@ export default function Home() {
       </main>
     </Fragment>
   );
+};
+
+export default Home;
+
+export async function getStaticProps() {
+  /** TODO: next api routes로 불러오기. 아래와 같이 데이터를 로컬파일로 불러올 일은 없기때문. */
+  const stores = (await import('../public/stores.json')).default;
+
+  return {
+    props: { stores }, // await import한 데이터를 prop으로 넘겨줌. Home의 prop으로 받아진다.
+    revalidate: 60 * 60, // 매장 데이터는 빠르게 바뀌는 값이 아니기 때문에 시간을 길게줌. 한시간
+  };
 }
