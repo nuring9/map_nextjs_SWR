3b5b12bb0dcb23c5bef68e0db2fe1deda220e5e1
Nuri <for9595@google.com>
2023-04-26 14:25:56 +0900

    DetailSection 애니메이션 구현하기

--- components/home/DetailSection.tsx
+++ components/home/DetailSection.tsx
@@ -1,14 +1,30 @@
-import styles from '../../styles/detail.module.scss';
+import { useState } from 'react';
+import useSWR from 'swr';
+import { CURRENT_STORE_KEY } from '@hooks/useCurrentStore';
 import { IoIosArrowUp } from 'react-icons/io';
+import type { Store } from 'types/store';
+import styles from '../../styles/detail.module.scss';
 
 const DetailSection = () => {
+  const { data: currentStore } = useSWR<Store>(CURRENT_STORE_KEY);
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <div className={styles.detailSection}>
+    <div
+      className={`${styles.detailSection} ${expanded ? styles.expanded : ''} ${
+        currentStore ? styles.selected : ''
+      }`}
+    >
       <div className={styles.header}>
-        <button className={styles.arrowButton} disabled>
+        <button
+          className={`${styles.arrowButton} ${expanded ? styles.expanded : ''}`}
+          onClick={() => setExpanded(!expanded)}
+          disabled={!currentStore}
+        >
           <IoIosArrowUp size={20} color="#666666" />
         </button>
-        <p className={styles.title}>매장을 선택해주세요</p>
+        {!currentStore && <p className={styles.title}>매장을 선택해주세요</p>}
+        {currentStore && <p className={styles.title}>{currentStore.name}</p>}
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,14 @@ const Home: NextPage<Props> = ({ stores }) => {
   return (
     <Fragment>
       <Header />
-      <main style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <main
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+        }}
+      >
         <MapSection />
         <DetailSection />
       </main>
